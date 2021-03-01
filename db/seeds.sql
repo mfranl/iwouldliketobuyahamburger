72a3fad5e4859484a07094721cb94956b2e0ccaa
mfranl <mflandry09@gmail.com>
2021-03-01 08:31:53 -0700

    added burger types into seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Peanut Butter & Jelly Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Crabby Patty Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Avocado Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', FALSE);
