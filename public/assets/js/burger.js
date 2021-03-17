556cba378c2c604e0d2307de4f7bc5ab6f014425
mfranl <mflandry09@gmail.com>
2021-03-17 11:33:03 -0600

    fixed error

---
+++ public/assets/js/burger.js
@@ -0,0 +1,95 @@
+document.addEventListener('DOMContentLoaded',(event)=>{
+    if(event){
+        console.log('DOM loaded');
+    }
+ 
+    const getDevoured = document.querySelectorAll('.has-eaten');
+   
+    if(getDevoured) {
+       
+        getDevoured.forEach((btn)=>{
+            btn.addEventListener('click',(e)=>{
+             
+                const id = e.target.getAttribute('data-id');
+                const gotDevoured = e.target.getAttribute('data-devoured')
+                
+                 let parsenumber = parseInt(gotDevoured) + 1;
+               
+                const devouredState = {
+                    devoured: parsenumber,
+                };
+
+                console.log(devouredState);
+
+             
+
+                fetch(`/api/burgers/${id}`,{
+                    
+                    method: 'PUT',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+
+                    },
+                   
+                    body: JSON.stringify(devouredState),
+
+                   
+                }).then((response)=>{
+                  
+                    if(response.ok) {
+                       
+                        console.log(`changed devoured to ${gotDevoured}`);
+                        
+                        location.reload('/');
+
+                    } else {
+                        alert('something went wrong');
+                    }
+                });
+            })
+        })
+    }
+
+    
+    const getBurgerBtn  = document.getElementById('create-burger');
+  
+    burger_names = document.getElementById("bgrs").value.trim(),
+   
+    dvs = document.getElementById('devoured').checked
+  
+
+    if(getBurgerBtn)  {
+       
+        getBurgerBtn.addEventListener('submit',(e)=>{
+
+            e.preventDefault();
+            
+        
+        const newBurger = { 
+           
+        burger_name: document.getElementById("bgrs").value.trim(),
+        devoured: document.getElementById('devoured').checked,
+        };
+       
+
+      
+        fetch('/api/burgers',{
+            method:'POST',
+            headers:{
+                Accept: 'application/json',
+                'Content-Type':'application/json',
+            },
+           
+            body: JSON.stringify(newBurger),
+        }).then(()=>{
+            
+            document.getElementById('bgrs').value = '';
+            console.log('Created a new burger');
+           
+            location.reload();
+        });
+    });
+
+    }
+})
