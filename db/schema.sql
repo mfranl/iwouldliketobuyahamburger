74f70513a74d25ba95f60179d594268bf25b1328
mfranl <mflandry09@gmail.com>
2021-03-17 08:43:58 -0600

    fixing table boolean

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
-    devoured BOOL NOT NULL,
+    devoured BOOLEAN NOT NULL,
     PRIMARY KEY (id)
 );
